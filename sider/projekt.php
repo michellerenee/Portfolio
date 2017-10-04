d4f60e30f32b23965f8942d64a1233feb92fa2ee
Michelle Renee <pigebarn94@hotmail.com>
2017-10-04 15:15:20 +0200

    Tilføjet projektside og lavet styling

--- sider/projekt.php
+++ sider/projekt.php
@@ -1,6 +1,17 @@
 <section>
+  <h1>Projektnavn</h1>
+  <article class="art1">
+    <p>Her er der en forklaring af projektet</p>
+    <h2>Krav</h2>
+    <p>Forklaring af kravene til opgaven</p>
+  </article>
 
+  <article class="art2">
+    billeder af projektet
+  </article>
+  <div class="block"></div>
 </section>
+
 <div class="bottom-stribe">
   <div class="first"></div>
   <div class="second"></div>
